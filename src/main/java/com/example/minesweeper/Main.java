a36619b95a17df27a7adccf1a659d7843f0f8120
Berk Ormanlı <berkormanl@gmail.com>
2023-05-12 21:48:50 +0300

    calisiyo iste

--- src/main/java/com/example/minesweeper/Main.java
+++ src/main/java/com/example/minesweeper/Main.java
@@ -4,139 +4,145 @@ import java.util.Random;
 import java.util.Scanner;
 
 class MineSweeper {
-
-    int rows;
-    int column;
-    int bombCount;
-    Cell [][] board;
     Scanner input = new Scanner(System.in);
     Random random = new Random();
+    int rows;
+    int columns;
+    int bombCount;
+    Cell [][]board;
 
-    void createBoard () {
-        bombCount = (rows * column) / 4;
-        board = new Cell[rows][column];
+    void createBoard() {
+        bombCount = (rows * columns) / 4;
+        board = new Cell[rows][columns];
 
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                board = new Cell[i][j];
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = new Cell();
             }
         }
 
         while (bombCount > 0) {
+
             int i = random.nextInt(0, rows);
-            int j = random.nextInt(0, column);
+            int j = random.nextInt(0, columns);
 
             if (!board[i][j].isHasBomb()) {
+
                 board[i][j].setHasBomb(true);
                 bombCount--;
-
             }
         }
+
         printMap();
 
         playGame();
     }
 
-    boolean checkWin(){
-        int unOpenedCells = 0;
+    boolean checkWin() {
+        int unopenedCells = 0;
         int bombNumber = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
 
-        for(int i =0; i<board.length; i++){
-            for(int j=0; j<board.length; j++){
-                if( board[i][j].getContent() == "-"){
-                    unOpenedCells++;
+                if (board[i][j].getContent() == "-") {
+                    unopenedCells++;
                 }
-                if(board[i][j].isHasBomb()){
+                if (board[i][j].isHasBomb()) {
                     bombNumber++;
                 }
-            }
-        }
 
-        return unOpenedCells == bombNumber;
+            }
+
+        }
+        return bombNumber == unopenedCells;
     }
 
-    void userInput(){
+    void userInput() {
+        System.out.println("Welcome to Mine Sweeper!");
 
-        System.out.println("Welcome to the mine sweeper!");
-
-        System.out.print("Please enter a rows number : ");
+        System.out.print("Please enter a rows number :");
         rows = input.nextInt();
-
-        System.out.print("Please enter a column number : ");
-        column = input.nextInt();
+        System.out.print("Please enter a column number :");
+        columns = input.nextInt();
         createBoard();
     }
+
     void playGame(){
         boolean finish = false;
+        while (!finish) {
+            System.out.print("Enter a row: ");
+            int rowInput=input.nextInt()-1;
 
-        while (!finish){
-            System.out.print("Please enter a rows :");
-            int newRows = input.nextInt();
-
-            System.out.print("Please enter a column : ");
-            int newColumn = input.nextInt();
+            System.out.print("Enter a column: ");
+            int columnInput=input.nextInt()-1;
 
             int bombCountOfCell = 0;
-            if((newRows > rows || newColumn > column) && (newRows < 0 || newColumn < 0) ){
+            if ((rowInput >= rows || columnInput >= columns) || (rowInput < 0 || columnInput < 0))
                 System.out.println("Row or column choice is out of bounds. Try again.");
-            } else if (board[newRows][newColumn].hasBomb) {
+            else if (board[rowInput][columnInput].isHasBomb()) {
                 System.out.println("Bomb exploded! You lose!");
                 printMineMap();
                 finish = true;
-            } else if (board[newRows][newColumn].getContent() != "-") {
+            }
+            else if (board[rowInput][columnInput].getContent() != "-") {
                 System.out.println("This sell is already selected. Try another cell.");
             }
-            else{
-                for(int i = newRows-1; i < newRows +2; i++){
-                    for (int j = newColumn -1; j < newColumn +2; j++ ){
-                        if((i >= 0 && j >= 0) && (i < rows && j < column) && board[i][j].hasBomb ){
+            else {
+                //if (board[rowInput][columnInput].getContent() == "-" && !board[rowInput][columnInput].isHasBomb()) {
+                for (int i = rowInput - 1; i < rowInput + 2; i++) {
+                    for (int j = columnInput - 1; j < columnInput + 2; j++) {
+
+                        if (i >= 0 && j >= 0 && i < rows && j < columns && board[i][j].isHasBomb()) {
                             bombCountOfCell++;
+                            //board[rowInput][columnInput].setContent(Integer.toString(bombCountOfCell));
                         }
+                        //else {
+                        //board[rowInput][columnInput].setContent(Integer.toString(bombCountOfCell));
+                        //}
                     }
                 }
-                board[newRows][newColumn].setContent(Integer.toString(bombCountOfCell));
+                board[rowInput][columnInput].setContent(Integer.toString(bombCountOfCell));
+                printMap();
 
-                if (checkWin()){
+                if (checkWin()) {
                     System.out.println("You win!");
                     printMineMap();
                     finish = true;
                 }
+                //}
             }
         }
     }
 
-    public void printMap(){
-        System.out.println("1");
-        for(int i = 0; i<board.length; i++){
-            for(int j = 0; j<board.length; j++){
-                System.out.println(board[i][j].getContent() + " ");
+    void printMap() {
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||");
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board[i].length; j++){
+                System.out.print(board[i][j].getContent() + " ");
             }
             System.out.println();
         }
-        System.out.println("2");
-
-
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||");
     }
 
-    public void printMineMap(){
-        System.out.println(" ");
-        for (int i = 0; i<board.length; i++){
-            for(int j = 0; j< board.length; j++){
-                if(board[i][j].hasBomb){
-                    System.out.println("* ");
+    void printMineMap() {
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||");
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board[i].length; j++){
+                if (board[i][j].isHasBomb()){
+                    System.out.print("* ");
                 }
                 else {
-                    System.out.println("- ");
+                    System.out.print(board[i][j].getContent()+" ");
                 }
-                System.out.println();
             }
-            System.out.println("2");
+            System.out.println();
         }
-
+        System.out.println("||||||||||||||||||||||||||||||||||||||||||||||");
     }
 }
 public class Main {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         MineSweeper ms = new MineSweeper();
         ms.userInput();
     }
